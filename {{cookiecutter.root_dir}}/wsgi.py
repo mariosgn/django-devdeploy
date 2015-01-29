831ee0a988f13f138bc99d37075e0ede30d1a2d1
Mario Signorino <mario.signorino@gnufish.net>
2015-01-30 00:59:29 +0100

    supervisord

--- {{cookiecutter.root_dir}}/wsgi.py
+++ {{cookiecutter.root_dir}}/wsgi.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{cookiecutter.project_name}}.settings.prod_sqlite")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{cookiecutter.project_name}}.settings.quick_prod")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
