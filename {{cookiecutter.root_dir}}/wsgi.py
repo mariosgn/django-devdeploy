9ac1e504df8a49cb32691015b995f73194e42069
Mario Signorino <mario.signorino@gnufish.net>
2015-01-29 22:09:42 +0100

    fixing deploy files

--- {{cookiecutter.root_dir}}/wsgi.py
+++ {{cookiecutter.root_dir}}/wsgi.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "valoreformazione.settings.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{cookiecutter.project_name}}.settings.prod")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
