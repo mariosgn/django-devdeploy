0e0e714a34f19d298efb2e71ced9f3dd534a54c4
Mario Signorino <mario.signorino@gnufish.net>
2015-01-30 01:05:03 +0100

    supervisord

--- {{cookiecutter.root_dir}}/bin/gunicorn_start.bash
+++ {{cookiecutter.root_dir}}/bin/gunicorn_start.bash
@@ -1,43 +1,32 @@
 #!/bin/bash
-set -x
+ 
+NAME="{{cookiecutter.project_name}}"            
+DJANGODIR={{cookiecutter.deploy_fullpath_dir}}/{{cookiecutter.domain_name}}          
+USER={{cookiecutter.webuser}}                                      
+GROUP={{cookiecutter.group}}                                   
+NUM_WORKERS=3                                   
+DJANGO_SETTINGS_MODULE={{cookiecutter.project_name}}.settings.quick_prod                    
+LOGDIR={{cookiecutter.deploy_fullpath_dir}}/{{cookiecutter.domain_name}}/log
 
-APPNAME="{{cookiecutter.project_name}}"            # Name of the application
-BASEDIR=/webapp
-HOSTNAME='fd1.floodream.com'
-
-
-
-DJANGODIR=$BASEDIR/$APPNAME                        # Django project directory
-#SOCKFILE=unix:$BASEDIR/run/gunicorn.sock          # we will communicte using this unix socket
-SOCKFILE=localhost:9000                            # we will communicte using this unix socket
-USER=webuser                                       # the user to run as
-GROUP=webapps                                      # the group to run as
-NUM_WORKERS=3                                      # how many worker processes should Gunicorn spawn
-DJANGO_SETTINGS_MODULE=$APPNAME.settings           # which settings file should Django use
-#DJANGO_WSGI_MODULE=$APPNAME.wsgi                  # WSGI module name
-DJANGO_WSGI_MODULE=wsgi
-PYTHON_VIRTUAL_ENV=$BASEDIR/pvenv
-
-echo "Starting $APPNAME as `whoami`"
+echo "Starting $NAME as `whoami`"
  
 # Activate the virtual environment
-cd $BASEDIR 
-source $PYTHON_VIRTUAL_ENV/bin/activate
-#export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
-export PYTHONPATH=$BASEDIR:$DJANGODIR:$PYTHONPATH
-export SECRET_KEY='mu7mzgqs&5@yfw&_9b_rr4x002c&hwfzqy&5(f)o*a&u_6a7zs'
-export LOCAL_HOSTNAME=$HOSTNAME
- 
-# Create the run directory if it doesn't exist
-#RUNDIR=$(dirname $SOCKFILE)
-#test -d $RUNDIR || mkdir -p $RUNDIR
+cd $DJANGODIR
+source venv/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+
+test -d $LOGDIR || mkdir -p $LOGDIR
  
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec $PYTHON_VIRTUAL_ENV/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
-  --name $APPNAME \
+exec ../bin/gunicorn \
+  --name $NAME \
   --workers $NUM_WORKERS \
-  --user=$USER --group=$GROUP \
+  --bind=127.0.0.1:{{cookiecutter.gunicorn_port}} \
   --log-level=debug \
-  --bind=$SOCKFILE
-
+  --pythonpath=minibugsproj \
+  wsgi:application
+#    --log-file=- \  
+#  --user=$USER --group=$GROUP \
