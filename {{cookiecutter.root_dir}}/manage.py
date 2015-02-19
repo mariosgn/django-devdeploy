f3d5e7a91274c3a9468a42c9511a5a46ee60105a
Mario Signorino <mario.signorino@gnufish.net>
2015-02-19 17:37:12 +0100

    new vagrant configuration

--- {{cookiecutter.root_dir}}/manage.py
+++ {{cookiecutter.root_dir}}/manage.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python
 import os
 import sys
+import pkgutil
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{cookiecutter.project_name}}.settings.dev")
+     if not "DJANGO_SETTINGS_MODULE" in os.environ:
+        print "DJANGO_SETTINGS_MODULE is not set:"
+        print "Please select one:"
+        
+        setts = [name for _, name, _ in pkgutil.iter_modules(['{{cookiecutter.project_name}}/settings'])]
+        for i, sett in enumerate(setts):
+            print "   %d: %s" % (i, sett)
+
+        s = raw_input("Please select the desired configuration (default: 0): ")    
+        
+        try:
+            s = int(s)
+        except ValueError:
+            s = 0
+        
+        settstr = "{{cookiecutter.project_name}}.settings.%s" % setts[s]
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", settstr)
+        
+        print '\033[31m' +"\nRun the followind line in your shell to avoid this question." + '\033[0m'
+        print '\033[32m' +"export DJANGO_SETTINGS_MODULE='%s'\n" % settstr + '\033[0m'
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", settstr)
 
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
--- {{cookiecutter.root_dir}}/{{cookiecutter.project_name}}/settings/dev_postgres.py
+++ {{cookiecutter.root_dir}}/{{cookiecutter.project_name}}/settings/dev_postgres.py
@@ -1,15 +1,20 @@
-
 from os import environ
 from dev import *
+import socket
 
+POSTGRESQL_DB_PORT = 5432
+
+#if we are outside the vagrant machine, we need to go to a different port
+if not "vagrant" in socket.gethostname():
+    POSTGRESQL_DB_PORT = 15432
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'floodream',
-        'USER': 'floodream',
+        'NAME': '{{cookiecutter.project_name}}',
+        'USER': '{{cookiecutter.project_name}}',
         'PASSWORD': 'dbpass',
         'HOST': 'localhost',
-        'PORT': '15432',
+        'PORT': POSTGRESQL_DB_PORT,
     }
 }
